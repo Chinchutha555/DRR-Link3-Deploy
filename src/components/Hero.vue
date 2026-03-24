a6469b9dc0e7115f7575f962dfde1f26667460ef
Chinchutha <Chinchutha.than@gmail.com>
2026-03-24 17:14:01 +0700

    delete

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -2,7 +2,7 @@
 import mainPhoto from "../assets/images/hero.svg";
 import { themeColor } from "../data/items";
 
-const heroHeading = "TEST";
+const heroHeading = "TEST Deploy จ้า";
 const buttonSeeFeature = "See Features";
 const buttonPricing = "Pricing";
 </script>
